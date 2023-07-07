1738a008a1c8cd079383a517adab9cf936af2d44
lnjiatao <jiatao@adsgency.ai>
2023-07-07 08:58:29 +0800

    fix: login api

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,7 +6,12 @@ export async function POST(req: Request) {
   const body = await req.json() as Partial<LoginForm>
   const res = await request('/login_api', 'POST', body)
   const data = await res.json()
-  return NextResponse.json(data, {
+  const response = NextResponse.json(data, {
     status: res.status,
   })
+  const setCookie = res.headers.get('set-cookie')
+  if (setCookie) {
+    response.headers.append('set-cookie', setCookie)
+  }
+  return response
 }
--- lib/request.ts
+++ lib/request.ts
@@ -3,12 +3,14 @@ type Method = 'GET' | 'POST'
 export async function request(url: string, method: Method, data: Record<string, any> = {}) {
   try {
     url = process.env.API_BASE_URL + url
+    // TODO: change to use application/json
+    const formData = new FormData()
+    for (const key in data) {
+      formData.append(key, data[key])
+    }
     const response = await fetch(url, {
       method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
+      body: formData,
     })
     return response
   } catch (error) {
