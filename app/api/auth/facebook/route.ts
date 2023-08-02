10d263ed13a2e439edc06ccef566bb90ee079d20
lnjiatao <138635001+lnjiatao@users.noreply.github.com>
2023-08-02 16:13:19 +0800

    Feat auth facebook (#51)
    * feat: add facebook callback
    
    * fix: request headers
    
    * update
    
    * update

--- app/api/auth/facebook/route.ts
+++ app/api/auth/facebook/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
   const code = new URL(req.url).searchParams.get('code')
-  const response = await fetch(`${process.env.API_BASE_URL}/fb_callback?code=${code}`, {
+  const response = await fetch(`${process.env.API_BASE_URL}/fb_callback_api?code=${code}`, {
     headers: {
       'cookie': req.headers.get('cookie') || '',
       'Access-Control-Allow-Origin': '*',
