c9c88e6abc1bb565221104dcaf19600e47e93131
lnjiatao <138635001+lnjiatao@users.noreply.github.com>
2023-08-02 14:52:55 +0800

    feat: add facebook callback (#48)

---
+++ app/api/auth/facebook/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+
+export async function GET(req: Request) {
+  const code = new URL(req.url).searchParams.get('code')
+  const response = await fetch(`${process.env.API_BASE_URL}/fb_callback?code=${code}`, {
+    headers: {
+      'cookie': req.headers.get('cookie') || '',
+    }
+  })
+  console.log('response.status', response.status)
+  const data = await response.json()
+  return NextResponse.json(data, {
+    status: 200,
+  })
+}
