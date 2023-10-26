769ee52ce0218aa32de6752bc57ef20fae5cfcbe
Sid From Adsgency.ai <148733276+sidfromadsgency@users.noreply.github.com>
2023-10-26 08:29:20 +0530

    Fix/revert to orginal (#147)
    * Fix(all) - revereted to old
    
    * Fix(Changed to V2)
    
    ---------
    
    Co-authored-by: Sid Narayanan <what-the-sid@Sids-MacBook-Pro.local>

--- app/auth/payment/PaymentForm/index.tsx
+++ app/auth/payment/PaymentForm/index.tsx
@@ -1,24 +1,28 @@
 'use client'
+// Imports
 import { paymentValidate } from '@/lib/validate';
 import { PaymentForm } from '@/types/auth';
 import { Icon } from '@iconify/react';
 import { FormikHelpers, useFormik } from 'formik';
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useState, useContext } from 'react'
 import styles from './payment.module.css'
 import CCInput from '@/components/CCInput';
 import ReactFlagsSelect from 'react-flags-select';
 import loadingIcon from '@iconify/icons-eos-icons/loading';
 import { useRouter } from 'next/navigation';
 import { DatePicker, message } from 'antd';
+import { AccountContext } from '@/context/account';
 import { SUCCESS_CODE } from '@/data/constant';
-import axios from '@/lib/axios';
 
 const PaymentForm = () => {
-
   const router = useRouter();
   const [messageApi, contextHolder] = message.useMessage();
   const [isLoading, setIsLoading] = useState(false);
   const [cardNumber, setCardNumber] = useState('');
+
+  const { nextPage, setCreditInfo } = useContext(AccountContext)
+
+  // Define formikForPayment using useFormik hook for handling form state.
   const formikForPayment = useFormik<PaymentForm>({
     initialValues: {
       card_number: '',
@@ -27,67 +31,74 @@ const PaymentForm = () => {
       cvc: '',
       country: 'US',
     },
-    onSubmit: onSubmitPayment,
-    validate: paymentValidate,
-  })
+    onSubmit: onSubmitPayment, // Function to handle form submission.
+    validate: paymentValidate, // Validation function for form fields.
+  });
 
+  // Function to handle form submission.
   async function onSubmitPayment(values: PaymentForm) {
-    const errors = Object.values(formikForPayment.errors)
+    const errors = Object.values(formikForPayment.errors);
     if (errors.length > 0) {
-      messageApi.error(errors[0])
-      return
+      messageApi.error(errors[0]);
+      return;
     }
     try {
       setIsLoading(true);
-      if (!isLoading) {
-        const response = await axios({
-          url: '/fapi/add_credit_api',
-          method: 'POST',
-          data: JSON.stringify({
-            ...values,
-            year: +values.expiration.split('-')[0],
-            month: +values.expiration.split('-')[1],
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        if (response.status === 200) {
-          const data = response.data
-          if (data.status === SUCCESS_CODE) {
-            messageApi.success(data.message || 'Add credit successfully');
-            setTimeout(() => {
-              router.push('/profile')
-            }, 500);
-          } else {
-            messageApi.error(data.message || 'Something went wrong');
-          }
+      const response = await fetch('/fapi/add_credit_api', {
+        method: 'POST',
+        body: JSON.stringify({
+          ...values,
+          year: +values.expiration.split('-')[0],
+          month: +values.expiration.split('-')[1],
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.status === SUCCESS_CODE) {
+          messageApi.success(data.message || 'Add credit successfully');
+          setTimeout(() => {
+            setCreditInfo(true)
+            if(nextPage && nextPage.length){
+              router.push(nextPage)
+            }
+            else{
+              router.push('/profile');
+            }
+          }, 500);
         } else {
-          messageApi.error(response.statusText || 'Something went wrong');
+          messageApi.error(data.message || 'Something went wrong');
         }
+      } else {
+        console.log('error: ', response.statusText);
+        messageApi.error(response.statusText || 'Something went wrong');
       }
     } catch (error) {
-      console.log('error: ', error)
+      console.log('error: ', error);
       messageApi.error(error as any || 'Something went wrong');
     } finally {
       setIsLoading(false);
     }
   }
 
+  // Function to handle change in CVC input.
   const handleChangeCVC = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    // 检查输入是否为整数
+    // Check if the input is an integer.
     if (!Number.isInteger(Number(value))) {
       return;
     }
-    // 检查输入的长度是否为4位
+    // Check if the input length is not more than 4 characters.
     if (value.length > 4) {
       return;
     }
-    // 更新输入的值
+    // Update the input value.
     formikForPayment.setFieldValue('cvc', value);
   };
 
+  // Function to handle change in expiration date.
   function onChangeExpiration(date: any, dateString: string) {
     formikForPayment.setFieldValue('expiration', dateString);
   }
@@ -107,7 +118,8 @@ const PaymentForm = () => {
             className={`${styles['payment-input']} ${formikForPayment.errors.card_holder && formikForPayment.touched.card_holder ? '!border-rose-600' : ''}`} {...formikForPayment.getFieldProps('card_holder')}
           />
         </div>
-        {/* card number */}
+
+        {/* Card Number */}
         <div className='flex flex-col gap-1'>
           <label className='text-primary-gray text-sm' htmlFor="card_number">
             Card number
@@ -117,7 +129,10 @@ const PaymentForm = () => {
             formikForPayment.setFieldValue('card_number', value.replaceAll(' ', ''));
           }} isError={!!formikForPayment.errors.card_number} />
         </div>
+
+        {/* Expiration Date and CVC */}
         <div className='flex gap-1 justify-between'>
+          {/* Expiration Date */}
           <div className='flex flex-col gap-1 max-sm:w-1/2'>
             <label className='text-primary-gray text-sm' htmlFor="expiration">
               Expiration
@@ -128,6 +143,7 @@ const PaymentForm = () => {
               onChange={onChangeExpiration}
               picker="month" />
           </div>
+          {/* CVC */}
           <div className='flex flex-col gap-1 max-sm:w-1/2'>
             <label className='text-primary-gray text-sm' htmlFor="cvc">
               CVC
@@ -142,9 +158,10 @@ const PaymentForm = () => {
             />
           </div>
         </div>
-        {/* country */}
+
+        {/* Country Selection */}
         <div className='flex flex-col gap-1'>
-          <label className='text-primary-gray text-sm' htmlFor="country">Country</label>
+        <label className='text-primary-gray text-sm' htmlFor="country">Country</label>
           <ReactFlagsSelect
             className={`${styles.flag} ${formikForPayment.errors.country && formikForPayment.touched.country ? '!border-rose-600' : ''}`}
             selected={formikForPayment.values.country}
@@ -155,6 +172,7 @@ const PaymentForm = () => {
             countries={["US"]}
           />
         </div>
+        {/* Submit Button */}
         <div className='flex items-center justify-between gap-2'>
           <button
             onClick={() => onSubmitPayment(formikForPayment.values)}
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Pricing } from '@/data/pricing'
 import AdminLayout from '@/layout/admin'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import checkIcon from '@iconify/icons-mdi/check';
 import closeIcon from '@iconify/icons-mdi/close';
 import { Icon } from '@iconify/react';
@@ -15,7 +15,6 @@ import { SubscriptionResp } from '@/types/account';
 import ConfirmModal from '@/app/pricing/ConfirmModal';
 import PayResult from '@/app/pricing/PayResult';
 import { useRouter } from 'next/navigation';
-import axios from '@/lib/axios';
 
 type SubscriptionButtonProps = {
   plan: PricingPlan,
@@ -66,7 +65,13 @@ const SubscriptionButton = ({ plan, onSubscription }: SubscriptionButtonProps) =
 
 const PricingPage = () => {
 
-  const { updateAccount, planId, isLogin } = useContext(AccountContext)
+  const { updateAccount,
+    planId,
+    isLogin,
+    creditInfo,
+    setNextPage,
+    setSelectedPlan,
+    selectedPlan} = useContext(AccountContext)
   const [buyPlanId, setBuyPlanId] = useState(0)
   const router = useRouter()
   const [plan, setPlan] = useState(Pricing[~~((planId - 1) / 3)].plan)
@@ -76,26 +81,35 @@ const PricingPage = () => {
   const [payResultVisible, setPayResultVisible] = useState(false)
   const [payResultMessage, setPayResultMessage] = useState('')
 
+  useEffect(() => {
+    if(selectedPlan && selectedPlan!==-1 && creditInfo){
+      setBuyPlanId(selectedPlan)
+      setConfirmVisible(true)
+    }
+  }, [])
+
   async function onSubscription(planId: number) {
     try {
       setLoading(true)
-      const response = await axios('/fapi/subscription_api', {
+      const response = await fetch('/fapi/subscription_api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        data: JSON.stringify({
+        body: JSON.stringify({
           plan_id: planId,
           mode: 'subscribe'
         })
       })
-      if (response.status === 200) {
-        const data: SubscriptionResp = response.data
+      if (response.ok) {
+        const data: SubscriptionResp = await response.json()
         if (data.status === 'active' || data.status === SUCCESS_CODE) {
           setPayResultMessage(data.message || 'Subscription successfully')
           setPayResultVisible(true)
           setConfirmVisible(false)
           updateAccount()
+          setSelectedPlan(-1)
+          setNextPage("")
         } else {
           messageApi.error(data.message || 'Something went wrong')
         }
@@ -112,18 +126,17 @@ const PricingPage = () => {
   async function onDowngrade(planId: number) {
     try {
       setLoading(true)
-      const response = await axios({
-        url: '/fapi/downgrade_subscription_api',
+      const response = await fetch('/fapi/downgrade_subscription_api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        data: JSON.stringify({
+        body: JSON.stringify({
           plan_id: planId,
         })
       })
-      if (response.status === 200) {
-        const data: SubscriptionResp = response.data;
+      if (response.ok) {
+        const data: SubscriptionResp = await response.json()
         if (data.status === SUCCESS_CODE || data.status === 'active') {
           setPayResultMessage(data.message || 'Downgrade successfully')
           setPayResultVisible(true)
@@ -145,19 +158,18 @@ const PricingPage = () => {
   async function onUpgrade(planId: number) {
     try {
       setLoading(true)
-      const response = await axios({
-        url: '/fapi/subscription_api',
+      const response = await fetch('/fapi/subscription_api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        data: JSON.stringify({
+        body: JSON.stringify({
           plan_id: planId,
           mode: 'upgrade'
         })
       })
-      if (response.status === 200) {
-        const data: SubscriptionResp = response.data;
+      if (response.ok) {
+        const data: SubscriptionResp = await response.json()
         if (data.status === 'active' || data.status === SUCCESS_CODE) {
           setPayResultMessage(data.message || 'Upgrade successfully')
           setPayResultVisible(true)
@@ -188,124 +200,133 @@ const PricingPage = () => {
     }
   }
 
-
   return (
-    <AdminLayout>
-      {contextHolder}
-      <ReactGATag
-        fieldObject={{
-          hitType: "pageview",
-          page: "/pricing",
-          title: "Pricing - AdsGency AI"
-        }}
-      />
-      <ConfirmModal
-        planId={buyPlanId}
-        visible={confirmVisible}
-        onCancel={() => setConfirmVisible(false)}
-        onOk={() => {
-          handleSubscription(buyPlanId)
-        }}
-        loading={loading}
-      />
-      <PayResult
-        visible={payResultVisible}
-        onClose={() => {
-          setPayResultVisible(false)
-          setPayResultMessage('')
-        }}
-        message={payResultMessage}
-      />
-      <div className='flex items-center gap-4'>
-        {
-          Pricing.map(price => (
-            <div
-              onClick={() => setPlan(price.plan)}
-              key={price.plan}
-              className={`${price.plan === plan ? 'border-primary-purple text-white' : 'border-transparent text-primary-gray'} border-2 rounded-lg bg-[#35363A] py-3 px-4 text-[18px] cursor-pointer flex items-center justify-center gap-2`}
-            >
-              <Image src={`/images/home/pricing/${price.plan === plan ? price.activeIcon : price.icon}`} width={24} height={24} alt={price.plan} />
-              <span>{price.plan}</span>
-            </div>
-          ))
-        }
-      </div>
-      <div className='mt-6 p-5 w-full h-min bg-[#1B1C21] border border-[#27282F] rounded-[12px]'>
-        <div className='flex flex-col'>
-          <div className='w-full flex justify-evenly items-center mb-5'>
-            <div className='w-[160px]' />
-            {
-              Pricing
-                .find(item => item.plan === plan)!
-                .plans
-                .map(item => (
-                  <div key={item.title}
-                    className='w-[240px] flex flex-col justify-center items-center bg-[#27282F] rounded-lg p-4 border border-[#3A3A3A] gap-2'
-                  >
-                    <div className='text-primary-purple uppercase'>{item.title}</div>
-                    <div className='flex items-center text-primary-gray text-sm'>
-                      <span className='text-white text-3xl mr-3'>{item.price}</span>
-                      <span>/month</span>
-                    </div>
-                    <SubscriptionButton
-                      plan={item}
-                      onSubscription={(planId) => {
-                        setBuyPlanId(planId)
-                        setConfirmVisible(true)
-                      }}
-                    />
-                  </div>
-                ))
-            }
-          </div>
+      <AdminLayout>
+        {contextHolder}
+        <ReactGATag
+          fieldObject={{
+            hitType: "pageview",
+            page: "/pricing",
+            title: "Pricing - AdsGency AI"
+          }}
+        />
+        <ConfirmModal
+          planId={buyPlanId}
+          visible={confirmVisible}
+          onCancel={() => setConfirmVisible(false)}
+          onOk={() => {
+            handleSubscription(buyPlanId)
+          }}
+          loading={loading}
+        />
+        <PayResult
+          visible={payResultVisible}
+          onClose={() => {
+            setPayResultVisible(false)
+            setPayResultMessage('')
+          }}
+          message={payResultMessage}
+        />
+        <div className='flex items-center gap-4'>
           {
-            Pricing
-              .find(item => item.plan === plan)!
-              ?.features
-              ?.map(item => (
-                <div key={item?.[0] as string}
-                  className='w-full h-10 flex justify-evenly items-center text-sm rounded-lg even:bg-[#27282F]'
-                >
-                  <div className='w-[160px] text-primary-gray text-sm text-left'>{item[0]}</div>
-                  <div className={`w-[240px] text-center flex items-center justify-center ${item[1] === 'Unlimited' || item[1] === 'Available' ? 'text-primary-purple' : ''}`}>
-                    {typeof item[1] === 'boolean' ? (
-                      item[1] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
-                    ) : item[1]}
-                  </div>
-                  <div className={`w-[240px] text-center flex items-center justify-center ${item[2] === 'Unlimited' || item[2] === 'Available' ? 'text-primary-purple' : ''}`}>
-                    {typeof item[2] === 'boolean' ? (
-                      item[2] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
-                    ) : item[2]}
-                  </div>
-                  <div className={`w-[240px] text-center flex items-center justify-center ${item[3] === 'Unlimited' || item[3] === 'Available' ? 'text-primary-purple' : ''}`}>
-                    {typeof item[3] === 'boolean' ? (
-                      item[3] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
-                    ) : item[3]}
-                  </div>
-                </div>
-              ))
+            Pricing.map(price => (
+              <div
+                onClick={() => setPlan(price.plan)}
+                key={price.plan}
+                className={`${price.plan === plan ? 'border-primary-purple text-white' : 'border-transparent text-primary-gray'} border-2 rounded-lg bg-[#35363A] py-3 px-4 text-[18px] cursor-pointer flex items-center justify-center gap-2`}
+              >
+                <Image src={`/images/home/pricing/${price.plan === plan ? price.activeIcon : price.icon}`} width={24} height={24} alt={price.plan} />
+                <span>{price.plan}</span>
+              </div>
+            ))
           }
-          <div
-            className='w-full mt-4 hidden justify-evenly items-center text-sm rounded-lg'
-          >
-            <div className='w-[160px]' />
+        </div>
+        <div className='mt-6 p-5 w-full h-min bg-[#1B1C21] border border-[#27282F] rounded-[12px]'>
+          <div className='flex flex-col'>
+            <div className='w-full flex justify-evenly items-center mb-5'>
+              <div className='w-[160px]' />
+              {
+                Pricing
+                  .find(item => item.plan === plan)!
+                  .plans
+                  .map(item => (
+                    <div key={item.title}
+                      className='w-[240px] flex flex-col justify-center items-center bg-[#27282F] rounded-lg p-4 border border-[#3A3A3A] gap-2'
+                    >
+                      <div className='text-primary-purple uppercase'>{item.title}</div>
+                      <div className='flex items-center text-primary-gray text-sm'>
+                        <span className='text-white text-3xl mr-3'>{item.price}</span>
+                        <span>/month</span>
+                      </div>
+                      <SubscriptionButton
+                        plan={item}
+                        onSubscription={(planId) => {
+                          if (!isLogin) {
+                            router.push('/login')
+                            return
+                          }
+                          if(!creditInfo){
+                            setSelectedPlan(planId)
+                            setNextPage("/pricing")
+                            router.push('/auth/payment')
+                            return
+                          }
+                          setBuyPlanId(planId)
+                          setConfirmVisible(true)
+                        }}
+                      />
+                    </div>
+                  ))
+              }
+            </div>
             {
               Pricing
                 .find(item => item.plan === plan)!
-                .plans
-                .map((item) => (
-                  <div key={item.price} className={`w-[240px] flex items-center justify-center`}>
-                    <SubscriptionButton
-                      plan={item}
-                      onSubscription={onSubscription}
-                    />
+                ?.features
+                ?.map(item => (
+                  <div key={item?.[0] as string}
+                    className='w-full h-10 flex justify-evenly items-center text-sm rounded-lg even:bg-[#27282F]'
+                  >
+                    <div className='w-[160px] text-primary-gray text-sm text-left'>{item[0]}</div>
+                    <div className={`w-[240px] text-center flex items-center justify-center ${item[1] === 'Unlimited' || item[1] === 'Available' ? 'text-primary-purple' : ''}`}>
+                      {typeof item[1] === 'boolean' ? (
+                        item[1] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
+                      ) : item[1]}
+                    </div>
+                    <div className={`w-[240px] text-center flex items-center justify-center ${item[2] === 'Unlimited' || item[2] === 'Available' ? 'text-primary-purple' : ''}`}>
+                      {typeof item[2] === 'boolean' ? (
+                        item[2] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
+                      ) : item[2]}
+                    </div>
+                    <div className={`w-[240px] text-center flex items-center justify-center ${item[3] === 'Unlimited' || item[3] === 'Available' ? 'text-primary-purple' : ''}`}>
+                      {typeof item[3] === 'boolean' ? (
+                        item[3] ? <Icon className='text-primary-purple' icon={checkIcon} /> : <Icon className='text-[#34A853]' icon={closeIcon} />
+                      ) : item[3]}
+                    </div>
                   </div>
                 ))
             }
+            <div
+              className='w-full mt-4 hidden justify-evenly items-center text-sm rounded-lg'
+            >
+              <div className='w-[160px]' />
+              {
+                Pricing
+                  .find(item => item.plan === plan)!
+                  .plans
+                  .map((item) => (
+                    <div key={item.price} className={`w-[240px] flex items-center justify-center`}>
+                      <SubscriptionButton
+                        plan={item}
+                        onSubscription={onSubscription}
+                      />
+                    </div>
+                  ))
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </AdminLayout>
+      </AdminLayout>
   )
 }
 
