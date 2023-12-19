f66bf6fc78f26fe72d96fda810e3809234630708
Jordan Jones <147287421+jordan-taylor-jones@users.noreply.github.com>
2023-12-19 01:19:39 -0600

    Feat/facebook insta integration (#259)
    * feat: facebook oauth and get pages api
    
    * fix: completed analyics dashboard of facebook
    
    * fix: eslint issue

--- app/api/planning/meta/launchAd/route.ts
+++ app/api/planning/meta/launchAd/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-const imghash = require('imghash');
+// const imghash = require('imghash');
 
 import { metaAxios, setMetaAccessToken } from "../index";
 
