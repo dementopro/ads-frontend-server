49a0023d2b2e6f54aff8a2af0c6fdc4b074bf0cc
yashashreempatel <145796898+yashashreempatel@users.noreply.github.com>
2023-12-21 13:00:24 -0600

    updated terms of services (#268)
    * Initiating terms of service page
    
    * Feat features page updated (#260)
    
    * initiating features page
    
    * adding step 1- 3 to features page
    
    * completed features page
    
    * Update FeaturesPageHome.tsx
    
    * Update FeaturesPageHome.tsx
    
    * Update step3.ts
    
    * Update step1.ts
    
    * Update Step5.tsx
    
    * Update Step4.tsx
    
    * Update Step3.tsx
    
    * Update Step2.tsx
    
    * Update Step1.tsx
    
    * fix: build error
    
    * Update Step4.tsx
    
    * Update NewsList.tsx
    
    * fix: alignment issue
    
    * added:new features page
    
    * news press bug fix
    
    * updated see more details button
    
    * landing page responsive (#261)
    
    * refactor: fixed or added many sections in landing page
    
    * respnsive landing page
    
    ---------
    
    Co-authored-by: jordan-taylor-jones <jordan@adsgency.ai>
    
    * added public pricing page (#262)
    
    * public pricing page added
    
    * fix:spacing
    
    * Feat/facebook insta integration (#263)
    
    * feat: facebook oauth and get pages api
    
    * fix: completed analyics dashboard of facebook
    
    * fix: eslint issue
    
    * feat: facebook and instagram launch
    
    * fix: npm run build errors of github workflow
    
    * fix: changed pricing subscribe link to signup page (#266)
    
    * updated terms of service page
    
    ---------
    
    Co-authored-by: jordan-taylor-jones <jordan@adsgency.ai>
    Co-authored-by: Jordan Jones <147287421+jordan-taylor-jones@users.noreply.github.com>

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -20,18 +20,18 @@ const PrivacyPolicyPage = () => {
         }}
       />
       <div className="ipad:px-[60px] desktop:px-[100px] android:pt-[100px] ipad:pt-[150px] pb-[32px] w-full relative z-10 bg-hero-pattern bg-cover">
-        <div className="desktop:w-[1240px] relative android:mx-[32px] android:my-[32px] ipad:mx-[60px] desktop:mx-auto ipad:my-[60px]">
+        <div className="desktop:w-[1240px] relative android:m-[32px] ipad:m-[60px] desktop:mx-auto">
           <Image
             width={100}
             height={100}
-            className='android:hidden ipad:block absolute top-[-20px] left-[-50px] android:w-[60px] ipad:w-[100px] h-auto z-10'
+            className='absolute android:top-[-10px] ipad:top-[-20px] android:left-[-30px] ipad:left-[-50px] android:w-[80px] ipad:w-[120px] desktop:w-[140px] h-auto z-10'
             title='Home'
             src={'/images/bg-elements/headline-circles.svg'} alt='logo'
           />
-          <div className="z-20 relative text-white font-poppins font-bold android:text-center ipad:text-left android:text-[34px] ipadmini:text-[44px] ipad:text-[54px] desktop:text-[64px] tracking-[-3px]">
+          <div className="z-20 relative text-white font-poppins font-bold text-left android:text-[34px] ipadmini:text-[44px] ipad:text-[54px] desktop:text-[64px] tracking-[-3px] android:leading-[43px] ipadmini:leading-[64px]">
             AdsGency AI Terms of Service
           </div>
-          <div className="z-20 relative text-[#D0D0D0] font-open-sans font-regular android:text-center ipad:text-left android:text-[12px] ipadmini:text-[16px] ipad:text-[20px] desktop:text-[24px]">
+          <div className="z-20 ipad:mt-[20px] relative text-[#D0D0D0] font-open-sans font-regular text-left android:text-[12px] ipadmini:text-[16px] ipad:text-[20px] desktop:text-[24px]">
             Last updated: December 2023
           </div>
         </div>
