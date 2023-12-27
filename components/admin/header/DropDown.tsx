19f36ac050a3b0e941fe528bc54400a2c94d12b9
Jordan Jones <147287421+jordan-taylor-jones@users.noreply.github.com>
2023-12-26 23:50:22 -0800

    Fix/tutorial btn color (#288)
    * fix: change email download type
    
    * feat: tutorial for navigation, home and seo features
    
    * feat: chat bot
    
    * feat: tutorials for email and social marketing
    
    * fix: changed color of tutorial button

--- components/admin/header/DropDown.tsx
+++ components/admin/header/DropDown.tsx
@@ -132,12 +132,12 @@ const DropDown = () => {
       {contextHolder}
       <div className="flex items-center">
         <span className="relative inline-flex mr-10">
-          <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-sky-500 bg-white dark:bg-slate-800 transition ease-in-out duration-150 ring-1 ring-slate-900/10 dark:ring-slate-200/20" onClick={handleOpenTutorial}>
+          <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-primary-purple transition ease-in-out duration-150 ring-1 ring-slate-900/10 dark:ring-slate-200/20" onClick={handleOpenTutorial}>
             Tutorial
           </button>
           <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-white"></span>
           </span>
         </span>
         {/* Create a dropdown menu with specified items */}
